3993ad94a4244a3225b99dc1f35416189d49b6a6
Ayanshabbir17 <107635174+Ayanshabbir17@users.noreply.github.com>
2022-12-30 20:48:04 +0530

    Delete test1.py

--- test1.py
+++
@@ -1,7 +0,0 @@
-def __init__(self, name):
-    self.foodapp_name = name
-    self.food = {}  # self.food= {1 : {---self.item--}, 2 : {------}, 3 : {------}}
-    self.foodid = len(self.food) + 1
-    self.admin_details = {}
-    self.user_details = {}
-    self.ordered_item = []
